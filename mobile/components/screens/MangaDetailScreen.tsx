e3151d3d481ce2ec6b397c94da3479c22b630bfd
Nertehaz2611 <hieunguyenthanh2611@gmail.com>
2025-06-02 16:22:38 +0700

    refactor: update favourites feature

--- mobile/components/screens/MangaDetailScreen.tsx
+++ mobile/components/screens/MangaDetailScreen.tsx
@@ -10,6 +10,7 @@ import { Chapter, Manga } from '../../types/Manga';
 import { StackNavigationProp } from '@react-navigation/stack';
 import api from '../../services/api';
 import CommentSection from '../CommentSection';
+import favoriteService from '../../services/favoriteService';
 
 type RootStackNavigationProp = StackNavigationProp<RootStackParamList>;
 
@@ -24,13 +25,14 @@ const MangaDetailScreen = () => {
   const [chapters, setChapters] = useState<Chapter[]>([]);
   const [loading, setLoading] = useState<boolean>(true);
   const [mangaInfor, setMangaInfor] = useState<Manga>(manga);
-  
+  const [isFavorite, setIsFavorite] = useState<boolean>(false);
 
   useEffect(() => {
     const fetchMangaInfor = async () => {
       try {
         const response = await api.get(`/manga/${mangaInfor.id}`);
         setMangaInfor(response.data);
+        setIsFavorite(response.data.isFavorite ?? false);
       } catch (error) {
         console.error('Error fetching chapters:', error);
       } finally {
@@ -51,6 +53,24 @@ const MangaDetailScreen = () => {
     fetchMangaInfor();
     fetchChapters();
   }, [mangaInfor.id]);
+
+  const toggleFavorite = async () => {
+    try {
+      if (isFavorite) {
+        // Xóa favorite
+        await favoriteService.removeFromFavorites(mangaInfor.id)
+        setIsFavorite(false);
+      } else {
+        // Thêm favorite
+        // await api.post(`/favourites/${mangaInfor.id}`);
+        await favoriteService.addToFavorites(mangaInfor.id)
+        setIsFavorite(true);
+      }
+    } catch (error) {
+      alert('Failed to update favorite status');
+      console.error(error);
+    }
+  };
 
   return (
     <LinearGradient {...GRADIENTS.BACKGROUND} style={styles.gradient}>
@@ -89,9 +109,9 @@ const MangaDetailScreen = () => {
 
           <TouchableOpacity
             style={styles.likeButton}
-            onPress={() => alert('you liked this manga')}
+            onPress={toggleFavorite}
           >
-            <Ionicons name="heart" size={24} color="white" />
+            <Ionicons name="heart" size={24} color={isFavorite ? 'red' : 'white'} />
           </TouchableOpacity>
         </View>
         <View style={{ marginTop: 5 }}>
